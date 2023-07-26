230b9480b1db99e6c83cc127376bbcb5766bfffc
arpit5220 <arpitkatiyar09197@gmail.com>
2023-07-26 11:05:25 +0530

    Diff_Of_Sum

---
+++ Diff_Of_Sum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Diff_Of_Sum
+{
+    public static void main(String[] args) {
+      
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n and m");
+        int n=sc.nextInt(); 
+        int m=sc.nextInt();
+
+        int s1=0,s2=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%m==0)
+            {
+                s1+=i;
+            }
+            else
+            {
+                s2+=i;
+            }
+        }
+
+        System.out.println(s2-s1);
+    }
+}
